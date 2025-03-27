c770e64baabbc4f940c51ecbe235c9071f3d8398
Chris Wyse <chris.wyse@wysechoice.net>
2025-03-27 01:15:24 -0400

    refactor(core): [P] modernize GTK usage in core
    (partial: 9e209ad3)
    
    Replay search branch commit 9535abdcf3e5 on the clean 3.1 development base with a standardized message. The rewritten subject summarizes this as: modernize GTK usage in core. It is part of the GTK modernization work.
    
    Original subject: WIP
    Original author: Chris Wyse <chris.wyse@wysechoice.net>
    Commit scope: core
    Changed paths: convert/fix_stocklabels.py
    
    Original search commit: 9535abdcf3e59b272f959f7ad09e5562396d4c81

--- convert/fix_stocklabels.py
+++ convert/fix_stocklabels.py
@@ -1,142 +1,25 @@
 from pathlib import Path
 from lxml import etree as ET
 
-# Replacement mapping for gtk stock labels and corresponding icon-names
-label_replacements = {
-    "gtk-close": ("Close", "window-close"),
-    "gtk-cancel": ("Cancel", "dialog-cancel"),
-    "gtk-ok": ("OK", "dialog-ok"),
-    "gtk-add": ("Add", "list-add"),
-    "gtk-connect": ("Connect", "network-connect"),
-    "gtk-remove": ("Remove", "list-remove"),
-    "gtk-refresh": ("Refresh", "view-refresh"),
-    "gtk-new": ("New", "document-new"),
-    "gtk-media-next": ("Next", "media-skip-forward"),
-    "gtk-media-previous": ("Previous", "media-skip-backward"),
-    "gtk-clear": ("Clear", "edit-clear"),
-    "gtk-execute": ("Run", "system-run"),
-    "gtk-properties": ("Properties", "document-properties"),
-    "gtk-preferences": ("Preferences", "preferences-system"),
-    "gtk-help": ("Help", "help-browser"),
-    "gtk-open": ("Open", "document-open"),
-    "gtk-save": ("Save", "document-save"),
-    "gtk-save-as": ("Save As", "document-save-as"),
-    "gtk-quit": ("Quit", "application-exit"),
-}
 
-def ensure_gtkbox_with_image_and_label(obj_new, icon_name, label_text):
-    label_prop_direct = obj_new.find("./property[@name='label']")
-    if label_prop_direct is not None:
-        obj_new.remove(label_prop_direct)
 
-    gtk_box = obj_new.find("./child/object[@class='GtkBox']")
-    if gtk_box is None:
-        child_box = ET.SubElement(obj_new, "child")
-        gtk_box = ET.SubElement(child_box, "object", {"class": "GtkBox"})
-        ET.SubElement(gtk_box, "property", name="orientation").text = "horizontal"
-        ET.SubElement(gtk_box, "property", name="visible").text = "True"
+def func_ensure_button_labels(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
+    for btn_old in tree_old.xpath(".//object[@class='GtkButton']"):
+        old_id = btn_old.get("id")
+        label_prop = btn_old.find("./property[@name='label']")
+        stock_prop = btn_old.find("./property[@name='use-stock']")
+        if (label_prop is not None or stock_prop is not None) and old_id:
+            btn_new = tree_new.xpath(f".//object[@class='GtkButton'][@id='{old_id}']")
+            if btn_new:
+                btn_new = btn_new[0]
+                label_direct = btn_new.find("./property[@name='label']")
+                if label_direct is None:
+                    label_direct = ET.SubElement(btn_new, "property", name="label")
+                    label_direct.text = label_prop.text if label_prop is not None else old_id.replace("_", " ").title()
 
-    for image_child in obj_new.findall("./child/object[@class='GtkImage']"):
-        obj_new.remove(image_child.getparent())
-        new_child = ET.SubElement(gtk_box, "child")
-        new_child.append(image_child)
 
-    for label_child in obj_new.findall("./child/object[@class='GtkLabel']"):
-        obj_new.remove(label_child.getparent())
-        new_child = ET.SubElement(gtk_box, "child")
-        new_child.append(label_child)
 
-    gtk_image = gtk_box.find("./child/object[@class='GtkImage']")
-    if gtk_image is None:
-        child_image = ET.SubElement(gtk_box, "child")
-        gtk_image = ET.SubElement(child_image, "object", {"class": "GtkImage"})
-    ET.SubElement(gtk_image, "property", name="visible").text = "True"
-    icon_prop = gtk_image.find("./property[@name='icon-name']")
-    if icon_prop is None:
-        icon_prop = ET.SubElement(gtk_image, "property", name="icon-name")
-    icon_prop.text = icon_name
-
-    gtk_label = gtk_box.find("./child/object[@class='GtkLabel']")
-    if gtk_label is None:
-        child_label = ET.SubElement(gtk_box, "child")
-        gtk_label = ET.SubElement(child_label, "object", {"class": "GtkLabel"})
-    ET.SubElement(gtk_label, "property", name="visible").text = "True"
-    label_prop = gtk_label.find("./property[@name='label']")
-    if label_prop is None:
-        label_prop = ET.SubElement(gtk_label, "property", name="label")
-    label_prop.text = label_text
-
-def update_images_from_stock(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
-    for img_old in tree_old.xpath(".//object[@class='GtkImage']"):
-        img_id = img_old.get("id")
-        stock_prop = img_old.find("./property[@name='stock']")
-        if stock_prop is not None and stock_prop.text:
-            stock_label = stock_prop.text.strip()
-            _, icon_name = label_replacements.get(stock_label, (None, None))
-            if icon_name:
-                img_new = tree_new.xpath(f".//object[@class='GtkImage'][@id='{img_id}']")
-                if img_new:
-                    img_new = img_new[0]
-                    icon_prop_new = img_new.find("./property[@name='icon-name']")
-                    if icon_prop_new is None:
-                        icon_prop_new = ET.SubElement(img_new, "property", name="icon-name")
-                    icon_prop_new.text = icon_name
-
-def map_attach_properties(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
-    for packing_old in tree_old.xpath(".//packing"):
-        obj_id = packing_old.getparent().get("id")
-        packing_new = tree_new.xpath(f".//object[@id='{obj_id}']/packing")
-        if packing_new:
-            packing_new = packing_new[0]
-            old_props = {prop.get("name"): int(prop.text) for prop in packing_old.findall("property") if prop.text.isdigit()}
-            if all(k in old_props for k in ["left_attach", "right_attach", "top_attach", "bottom_attach"]):
-                left = old_props["left_attach"]
-                right = old_props["right_attach"]
-                top = old_props["top_attach"]
-                bottom = old_props["bottom_attach"]
-
-                for name, value in [("left-attach", left), ("top-attach", top),
-                                    ("width", right - left), ("height", bottom - top)]:
-                    prop = packing_new.find(f"./property[@name='{name}']")
-                    if prop is None:
-                        prop = ET.SubElement(packing_new, "property", name=name)
-                    prop.text = str(value)
-
-def modify_labels(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
-    pass  # existing logic here
-
-# Set of known valid GTK 3.24+ property names
-valid_gtk_properties = {
-    'can-focus', 'receives-default', 'has-focus', 'is-focus', 'visible',
-    'sensitive', 'orientation', 'spacing', 'expand', 'fill', 'position',
-    'border-width', 'always-show-image', 'use-underline', 'label', 
-    'icon-name', 'pixel-size', 'halign', 'valign', 'width-request', 
-    'height-request', 'left-attach', 'top-attach', 'width', 'height',
-    'margin-start', 'margin-end', 'margin-top', 'margin-bottom',
-    'hexpand', 'vexpand', 'tooltip-text', 'editable', 'wrap-mode',
-    'wrap', 'selectable', 'stock'
-}
-
-def map_property_names(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
-    for obj in tree_new.xpath(".//object"):
-        obj_class = obj.get('class')
-        obj_id = obj.get('id')
-        props_to_remove = []
-        for prop in obj.xpath(".//property[contains(@name, '_')]"):
-            original_name = prop.get('name')
-            updated_name = original_name.replace('_', '-')
-            if updated_name in valid_gtk_properties:
-                prop.set('name', updated_name)
-            else:
-                print(f"Warning: Removed deprecated property '{original_name}' from '{obj_class}' id '{obj_id}'")
-                props_to_remove.append(prop)
-        
-        # Remove properties safely after iteration
-        for prop in props_to_remove:
-            if prop.getparent() is obj:
-                obj.remove(prop)
-
-def handle_deprecated_properties(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
+def func_handle_deprecated_properties(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
     deprecated_mappings = {
         'rules-hint': None,
         'stock': 'icon-name',
@@ -199,17 +82,48 @@ def handle_deprecated_properties(tree_old: ET.ElementTree, tree_new: ET.ElementT
             if prop.getparent() is obj:
                 obj.remove(prop)
 
-WIDGET_SHADOW_TYPES = {}
+def func_map_attach_properties(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
+    for packing_old in tree_old.xpath(".//packing"):
+        obj_id = packing_old.getparent().get("id")
+        packing_new = tree_new.xpath(f".//object[@id='{obj_id}']/packing")
+        if packing_new:
+            packing_new = packing_new[0]
+            old_props = {prop.get("name"): int(prop.text) for prop in packing_old.findall("property") if prop.text.isdigit()}
+            if all(k in old_props for k in ["left_attach", "right_attach", "top_attach", "bottom_attach"]):
+                left = old_props["left_attach"]
+                right = old_props["right_attach"]
+                top = old_props["top_attach"]
+                bottom = old_props["bottom_attach"]
 
-shadow_legacy_to_css = {
-    "none": "shadow-none",
-    "in": "shadow-in",
-    "out": "shadow-out",
-    "etched-in": "shadow-etched-in",
-    "etched-out": "shadow-etched-out",
-}
+                for name, value in [("left-attach", left), ("top-attach", top),
+                                    ("width", right - left), ("height", bottom - top)]:
+                    prop = packing_new.find(f"./property[@name='{name}']")
+                    if prop is None:
+                        prop = ET.SubElement(packing_new, "property", name=name)
+                    prop.text = str(value)
 
-def map_shadow_type(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
+
+def func_map_property_names(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
+    for obj in tree_new.xpath(".//object"):
+        obj_class = obj.get('class')
+        obj_id = obj.get('id')
+        props_to_remove = []
+        for prop in obj.xpath(".//property[contains(@name, '_')]"):
+            original_name = prop.get('name')
+            updated_name = original_name.replace('_', '-')
+            if updated_name in valid_gtk_properties:
+                prop.set('name', updated_name)
+            else:
+                print(f"Warning: Removed deprecated property '{original_name}' from '{obj_class}' id '{obj_id}'")
+                props_to_remove.append(prop)
+        
+        # Remove properties safely after iteration
+        for prop in props_to_remove:
+            if prop.getparent() is obj:
+                obj.remove(prop)
+
+
+def func_map_shadow_type(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
     for old_widget in tree_old.xpath(".//property[@name='shadow_type']"):
         shadow_type_value = old_widget.text.strip()
         parent_old = old_widget.getparent()
@@ -239,22 +153,46 @@ def map_shadow_type(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
             else:
                 print(f"Warning: Invalid shadow_type '{shadow_type_value}' for '{widget_class}' id '{new_id}'")
 
-def ensure_button_labels(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
+def func_modify_labels(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
     for btn_old in tree_old.xpath(".//object[@class='GtkButton']"):
         old_id = btn_old.get("id")
         label_prop = btn_old.find("./property[@name='label']")
-        stock_prop = btn_old.find("./property[@name='use-stock']")
-        if (label_prop is not None or stock_prop is not None) and old_id:
-            btn_new = tree_new.xpath(f".//object[@class='GtkButton'][@id='{old_id}']")
-            if btn_new:
-                btn_new = btn_new[0]
-                label_direct = btn_new.find("./property[@name='label']")
-                if label_direct is None:
-                    label_direct = ET.SubElement(btn_new, "property", name="label")
-                    label_direct.text = label_prop.text if label_prop is not None else old_id.replace("_", " ").title()
+        stock_label = btn_old.find("./property[@name='stock']")
+        if stock_label is not None:
+            stock = stock_label.text.strip()
+        else:
+            stock = None
+
+        use_stock = btn_old.find("./property[@name='use-stock']")
+        use_stock_val = use_stock.text.strip().lower() == "true" if use_stock is not None else False
+
+        btn_new = tree_new.xpath(f".//object[@class='GtkButton'][@id='{old_id}']")
+        if not btn_new:
+            continue
+
+        btn_new = btn_new[0]
+
+        label_text, icon_name = None, None
+        if stock in label_replacements:
+            label_text, icon_name = label_replacements[stock]
+        elif label_prop is not None:
+            label_text = label_prop.text
+        else:
+            label_text = old_id.replace("_", " ").title()
+
+        if use_stock_val and stock in label_replacements:
+            # full stock emulation
+            ensure_gtkbox_with_image_and_label(btn_new, icon_name, label_text)
+        elif label_text:
+            label_direct = btn_new.find("./property[@name='label']")
+            if label_direct is None:
+                label_direct = ET.SubElement(btn_new, "property", name="label")
+            label_direct.text = label_text
 
 
-def update_images_from_stock(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
+
+
+def func_update_images_from_stock(tree_old: ET.ElementTree, tree_new: ET.ElementTree):
     for img_old in tree_old.xpath(".//object[@class='GtkImage']"):
         img_id = img_old.get("id")
         stock_prop = img_old.find("./property[@name='stock']")
@@ -270,6 +208,96 @@ def update_images_from_stock(tree_old: ET.ElementTree, tree_new: ET.ElementTree)
                         icon_prop_new = ET.SubElement(img_new, "property", name="icon-name")
                     icon_prop_new.text = icon_name
 
+
+
+def func_ensure_gtkbox_with_image_and_label(obj_new, icon_name, label_text):
+    label_prop_direct = obj_new.find("./property[@name='label']")
+    if label_prop_direct is not None:
+        obj_new.remove(label_prop_direct)
+
+    gtk_box = obj_new.find("./child/object[@class='GtkBox']")
+    if gtk_box is None:
+        child_box = ET.SubElement(obj_new, "child")
+        gtk_box = ET.SubElement(child_box, "object", {"class": "GtkBox"})
+        ET.SubElement(gtk_box, "property", name="orientation").text = "horizontal"
+        ET.SubElement(gtk_box, "property", name="visible").text = "True"
+
+    for image_child in obj_new.findall("./child/object[@class='GtkImage']"):
+        obj_new.remove(image_child.getparent())
+        new_child = ET.SubElement(gtk_box, "child")
+        new_child.append(image_child)
+
+    for label_child in obj_new.findall("./child/object[@class='GtkLabel']"):
+        obj_new.remove(label_child.getparent())
+        new_child = ET.SubElement(gtk_box, "child")
+        new_child.append(label_child)
+
+    gtk_image = gtk_box.find("./child/object[@class='GtkImage']")
+    if gtk_image is None:
+        child_image = ET.SubElement(gtk_box, "child")
+        gtk_image = ET.SubElement(child_image, "object", {"class": "GtkImage"})
+    ET.SubElement(gtk_image, "property", name="visible").text = "True"
+    icon_prop = gtk_image.find("./property[@name='icon-name']")
+    if icon_prop is None:
+        icon_prop = ET.SubElement(gtk_image, "property", name="icon-name")
+    icon_prop.text = icon_name
+
+    gtk_label = gtk_box.find("./child/object[@class='GtkLabel']")
+    if gtk_label is None:
+        child_label = ET.SubElement(gtk_box, "child")
+        gtk_label = ET.SubElement(child_label, "object", {"class": "GtkLabel"})
+    ET.SubElement(gtk_label, "property", name="visible").text = "True"
+    label_prop = gtk_label.find("./property[@name='label']")
+    if label_prop is None:
+        label_prop = ET.SubElement(gtk_label, "property", name="label")
+    label_prop.text = label_text
+
+
+# Replacement mapping for gtk stock labels and corresponding icon-names
+label_replacements = {
+    "gtk-close": ("Close", "window-close"),
+    "gtk-cancel": ("Cancel", "dialog-cancel"),
+    "gtk-ok": ("OK", "dialog-ok"),
+    "gtk-add": ("Add", "list-add"),
+    "gtk-connect": ("Connect", "network-connect"),
+    "gtk-remove": ("Remove", "list-remove"),
+    "gtk-refresh": ("Refresh", "view-refresh"),
+    "gtk-new": ("New", "document-new"),
+    "gtk-media-next": ("Next", "media-skip-forward"),
+    "gtk-media-previous": ("Previous", "media-skip-backward"),
+    "gtk-clear": ("Clear", "edit-clear"),
+    "gtk-execute": ("Run", "system-run"),
+    "gtk-properties": ("Properties", "document-properties"),
+    "gtk-preferences": ("Preferences", "preferences-system"),
+    "gtk-help": ("Help", "help-browser"),
+    "gtk-open": ("Open", "document-open"),
+    "gtk-save": ("Save", "document-save"),
+    "gtk-save-as": ("Save As", "document-save-as"),
+    "gtk-quit": ("Quit", "application-exit"),
+}
+
+# Set of known valid GTK 3.24+ property names
+valid_gtk_properties = {
+    'can-focus', 'receives-default', 'has-focus', 'is-focus', 'visible',
+    'sensitive', 'orientation', 'spacing', 'expand', 'fill', 'position',
+    'border-width', 'always-show-image', 'use-underline', 'label', 
+    'icon-name', 'pixel-size', 'halign', 'valign', 'width-request', 
+    'height-request', 'left-attach', 'top-attach', 'width', 'height',
+    'margin-start', 'margin-end', 'margin-top', 'margin-bottom',
+    'hexpand', 'vexpand', 'tooltip-text', 'editable', 'wrap-mode',
+    'wrap', 'selectable', 'stock'
+}
+
+WIDGET_SHADOW_TYPES = {}
+
+shadow_legacy_to_css = {
+    "none": "shadow-none",
+    "in": "shadow-in",
+    "out": "shadow-out",
+    "etched-in": "shadow-etched-in",
+    "etched-out": "shadow-etched-out",
+}
+
 def output_shadow_types():
     with open("widget_shadow_types.py", "w", encoding="utf-8") as file:
         file.write("WIDGET_SHADOW_TYPES = {\n")
@@ -278,13 +306,14 @@ def output_shadow_types():
         file.write("}\n")
 
 modification_functions = [
-    modify_labels,
-    update_images_from_stock,
-    map_attach_properties,
-    map_property_names,
-    handle_deprecated_properties,
-    ensure_button_labels,
-    map_shadow_type
+func_ensure_button_labels,
+func_handle_deprecated_properties,
+func_map_attach_properties,
+func_map_property_names,
+func_map_shadow_type,
+func_modify_labels,
+func_update_images_from_stock,
+func_update_images_from_stock,
 ]
 
 base_old = Path("/home/chris/repositories/ghini.desktop.new/bauble")
